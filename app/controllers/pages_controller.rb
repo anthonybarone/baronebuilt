add08db4dee68046dec47fa6305efb871f6312d7
anthonybarone <barone42@gmail.com>
2011-11-02 21:28:15 -0400

    configured homepage root route and tab urls

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,16 @@ class PagesController < ApplicationController
   def home
   end
 
+  def neighborhoods
+  end
+  
+  def model_home
+  end
+
   def photo_gallery
+  end
+
+  def contact
   end
 
 end
